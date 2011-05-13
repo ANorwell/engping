c41014cf7155a7a04dc84603cd56d2bddbca62d4
Arron Norwell <anorwell@gmail.com>
2011-05-13 18:49:49 -0400

    works poorly now...

--- index.php
+++ index.php
@@ -1,65 +1,154 @@
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Translate API Example</title>
- <script src="https://www.google.com/jsapi?key=ABQIAAAAvBr3E5gjsEcbIKWhctpmAxRpQYh-aYtvQo0i7nvVd6d9bhl1exQlUHxuWciZUEMKMNoGYoRj8jqX3w" type="text/javascript"></script>
+    <title>English => Pinglish</title>
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta> 
+    <script src="https://www.google.com/jsapi?key=ABQIAAAAvBr3E5gjsEcbIKWhctpmAxRpQYh-aYtvQo0i7nvVd6d9bhl1exQlUHxuWciZUEMKMNoGYoRj8jqX3w" type="text/javascript"></script>
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
+ <script type="text/javascript" src="../jqueryui/js/jquery-ui-1.8.4.custom.min.js"></script>
 
-  </head>
+ <style type="text/css">
+   body {
+               font-family:sans-serif;
+               margin: 0;
+               }
+  h3 { margin:0; }
+  h4 { margin:0; }  
+ </style>
+ 
+ </head>
   <body>
-    <h3>Source</h3>
-    <div id="sourceText">Hello world</div>
-
-    <h3>Translation</h3>
-    <div id="translation"></div>
-
-    <h3>Diacritization</h3>
-    <div id="diacritize"></div>
-
-    <h3>Transliteration</h3>
-    <div id="transliteration"></div>
     
+    <h3>Enter English phrase to translate:</h3>
+    <textarea id="sourceText" rows="2" cols="50">Hello</textarea>
+
+    <div>
+      <button id="translate" onClick="startTranslating()">Translate</button>
+    </div>
+
+    <br/>
+        
+    <div>
+      <h3>Translation:</h3>
+      <div id="transliteration"><br/></div>
+      <a id="infolink" style="font-size:0.6em">[info]</a>
+    </div>
+
+    <div id="info" style="float:left;border-style:solid;border-width:1px;font-size:small;padding:1em;">
+      <h4>Farsi Translation</h4>
+      <div id="translation"></div>
+      <h4>Diacritization</h4>
+      <div id="diacritize"></div>
+    </div>
+
+
     <script type="text/javascript">
-
-    //TODO: diacretize using diacretization api
-    //http://en.wikipedia.org/wiki/Romanization_of_Persian#Transliteration
-
-    
-    
-      function translateText(response) {
-          var trText = response.data.translations[0].translatedText;
-        document.getElementById("translation").innerHTML += "<br>" + trText;
-
-          var container = document.getElementById("transliteration");
-          
-          google.language.transliterate([trText], "fa", "en", function(result) {
-              if (!result.error) {
-
-                  if (result.transliterations && result.transliterations.length > 0 &&
-                      result.transliterations[0].transliteratedWords.length > 0) {
-                      container.innerHTML += result.transliterations[0].transliteratedWords[0];
-                  }
-              }  else {
-                  container.innerHTML += "error: " + result.error;
-              }
-          });
-      }
-
-    </script>
-    <script type="text/javascript">
+      console.log = function() {}; //don't show debug statements
+      $("#info").hide();
+      $("#translate").button();
+      $("#infolink").hover(function() { $("#info").show() }, function() { $("#info").hide() } );
+      
+      var apiKey = 'AIzaSyBx8ygu04GV6xUZK7rZuRerbyghA6lPVSw';
       google.load("language", "1");
-      google.load("elements", "1", {packages: "transliteration", "nocss" : true});
-      function go() {
-          var targetLang = 'fa';
-          var newScript = document.createElement('script');
-          newScript.type = 'text/javascript';
-          var sourceText = escape(document.getElementById("sourceText").innerHTML);
-          var source = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyBx8ygu04GV6xUZK7rZuRerbyghA6lPVSw&source=en&target=' + targetLang + '&callback=translateText&q=' + sourceText;
-          newScript.src = source;
-          
-          // When we add this script to the head, the request is sent off.
-          document.getElementsByTagName('head')[0].appendChild(newScript);
+
+      var trText;
+      var trMap = {
+          '%u0626' : 'y',
+          '%u0627' : 'a',
+          '%u0628' : 'b',
+          '%u0629' : '?',
+          '%u062A' : 't',
+          '%u062B' : 's',          
+          '%u062C' : 'j',
+          '%u062D' : 'h',
+          '%u062E' : 'kh',
+          '%u062F' : 'd',
+          '%u0630' : 'z',
+          '%u0631' : 'r',
+          '%u0632' : 'z',
+          '%u0633' : 's',
+          '%u0634' : 'sh',
+          '%u0635' : 's',
+          '%u0636' : 'z',
+          '%u0637' : 't',
+          '%u0638' : 'z',
+          '%u0639' : 'a',
+          '%u063A' : 'gh',
+          '%u063B' : '?',
+          '%u063C' : '?',
+          '%u063D' : '?',
+          '%u063E' : '?',
+          '%u063F' : '?',
+          '%u0640' : '?',
+          '%u0641' : 'f',
+          '%u0642' : 'gh',
+          '%u0643' : '?',
+          '%u0644' : 'l',
+          '%u0645' : 'm',
+          '%u0646' : 'n',
+          '%u0647' : 'h',
+          '%u0648' : 'v',
+          '%u0649' : 'y',
+          '%u064A' : 'y',
+
+          //the vowel marks
+          '%u064E' : 'a',
+          '%u064F' : 'o',
+          '%u0650' : "e",
+          '%u0651' : '',  //the w thing means nothing
+          '%u0652' : "'",
+          '%u06CC' : 'i'
+      };
+
+      function transliterate(str) {
+          var tr = '';
+          for (c in str) {
+              console.log(str[c] + ' : ' + escape(str[c]));
+              var utf8code = escape(str[c]);
+              if (utf8code in trMap) {
+                  tr += trMap[utf8code];
+              } else {
+                  tr += str[c];
+              }
+          }
+          return tr;
       }
 
-      google.setOnLoadCallback(go);
+      
+      //The callback to handle the response object with diacritized text.
+      function diacritResponse(response) {
+          console.log(escape(trText));
+          console.log(escape(response.data.diacritized_text));
+
+          
+          $("#diacritize").html(response.data.diacritized_text);
+          $("#transliteration").html(transliterate(response.data.diacritized_text));
+      }
+
+      //The callback to handle the response object with tranlated text.
+      function translateText(response) {
+          //TODO: diacretize using diacretization api
+          //http://en.wikipedia.org/wiki/Romanization_of_Persian#Transliteration
+
+          trText = response.data.translations[0].translatedText;
+          $("#translation").html("<br>" + trText);
+
+          var diacrit = $("#diacritize");
+
+          var diaURL = 'https://www.googleapis.com/language/diacritize/v1?key=' + apiKey +
+              '&lang=ar&last_letter=false&prettyprint=false&message=' + trText + '&callback=diacritResponse';
+
+          $.get(diaURL, function() {}, 'jsonp' );
+      }
+
+
+      function startTranslating() {
+          var targetLang = 'fa';
+          var sourceText = escape($("#sourceText").val());
+          var trURL = 'https://www.googleapis.com/language/translate/v2?key=' + apiKey +
+              '&source=en&target=' + targetLang + '&callback=translateText&q=' + sourceText;
+
+          $.get(trURL, function() {}, 'jsonp'); 
+      }
       
     </script>
   </body>
