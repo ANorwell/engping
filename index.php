a9cb3b6e88d6076f96e0f8caeb13ddad5a4f832f
Arron Norwell <anorwell@gmail.com>
2011-05-16 01:24:55 -0400

    added farsi=>eng translation

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 
  <style type="text/css">
    body {
-               font-family:sans-serif;
-               margin: 0;
-               }
+          font-family:sans-serif;
+          margin: 0;
+          }
   h3 { margin:0; }
   h4 { margin:0; }  
  </style>
@@ -18,7 +18,9 @@
  </head>
   <body>
     
-    <h3>Enter English phrase to translate:</h3>
+    <h3 id="trTitle" style="display:inline">Translate: </h3>
+    <a id="langs" title="Current translation direction">English To Transliterated Farsi</a>
+    <a id="switch" title="Click to change translation direction">&#8644;</a><br/>
     <textarea id="sourceText" rows="2" cols="50">Hello</textarea>
 
     <div>
@@ -29,29 +31,23 @@
         
     <div>
       <h3>Translation:</h3>
-      <div id="transliteration"><br/></div>
+      <div id="goaltext"><br/></div>
       <a id="infolink" style="font-size:0.6em">[info]</a>
     </div>
 
     <div id="info" style="float:left;border-style:solid;border-width:1px;font-size:small;padding:1em;">
-      <h4>Farsi Translation</h4>
-      <div id="translation"></div>
+      <h4>Farsi</h4>
+      <div id="farsi">n/a</div>
       <h4>Diacritization</h4>
-      <div id="diacritize"></div>
+      <div id="diacritize">n/a</div>
     </div>
 
 
     <script type="text/javascript">
-      console.log = function() {}; //don't show debug statements
-      $("#info").hide();
-      $("#translate").button();
-      $("#infolink").hover(function() { $("#info").show() }, function() { $("#info").hide() } );
-      
-      var apiKey = 'AIzaSyBx8ygu04GV6xUZK7rZuRerbyghA6lPVSw';
-      google.load("language", "1");
-
-      var trText;
       var trMap = {
+          '%u060C' : ',',
+          '%u061F' : '?',
+
           '%u0626' : 'y',
           '%u0627' : 'a',
           '%u0628' : 'b',
@@ -96,10 +92,65 @@
           '%u0650' : "e",
           '%u0651' : '',  //the w thing means nothing
           '%u0652' : "'",
-          '%u06CC' : 'i'
+          '%u06CC' : 'i',
+
+          '%u067E' : 'p',
+          '%u0686' : 'ch',
+          '%u0698' : 'zh',              
+          '%u06A9' : 'k',
+          '%u06AF' : 'g',
       };
 
-      function transliterate(str) {
+      var srcLang = 'en';
+      var dstLang = 'fa';
+      var isInfoShown = 0;
+      var trText;
+      
+      console.log = function() {}; //don't show debug statements
+
+
+      var apiKey = 'AIzaSyBx8ygu04GV6xUZK7rZuRerbyghA6lPVSw';
+      google.load("language", "1");
+      google.load("elements", "1", {
+          packages: "transliteration"
+      });
+
+      //set up the page interface
+      $("#info").hide();
+      $("#translate").button();
+      $("#switch").click(switchLangs);
+      $("#infolink").click(function() {
+          if (isInfoShown) {
+              $("#info").hide();
+              isInfoShown = 0;
+          } else {
+              $("#info").show();
+              isInfoShown = 1;
+          }
+      });
+
+      function switchLangs() {
+          if (srcLang == 'en') {
+              srcLang = 'fa';
+              dstLang = 'en';
+              $("#langs").html("Transliterated Farsi to English");
+          } else {
+              srcLang = 'en';
+              dstLang = 'fa';
+              $("#langs").html("English to Transliterated Farsi");
+          }
+          $("#info").hide();
+          $("#farsi").html("n/a");
+          $("#diacritize").html("n/a");
+          
+      };
+
+      /////////////////////
+      //Functions for translating from Pinglish to English
+      ///////////////////////
+      
+      //this acts on diacritized persian text!
+      function translitPrToPing(str) {
           var tr = '';
           for (c in str) {
               console.log(str[c] + ' : ' + escape(str[c]));
@@ -113,42 +164,77 @@
           return tr;
       }
 
-      
       //The callback to handle the response object with diacritized text.
-      function diacritResponse(response) {
+      function callbackDiacrit(response) {
           console.log(escape(trText));
           console.log(escape(response.data.diacritized_text));
-
-          
           $("#diacritize").html(response.data.diacritized_text);
-          $("#transliteration").html(transliterate(response.data.diacritized_text));
+          $("#goaltext").html(translitPrToPing(response.data.diacritized_text));
       }
 
-      //The callback to handle the response object with tranlated text.
-      function translateText(response) {
+      //The callback to handle the response object with tranlated persian text.
+      function callbackPrToPing(response) {
           //TODO: diacretize using diacretization api
           //http://en.wikipedia.org/wiki/Romanization_of_Persian#Transliteration
 
           trText = response.data.translations[0].translatedText;
-          $("#translation").html("<br>" + trText);
-
-          var diacrit = $("#diacritize");
+          $("#farsi").html(trText);
 
           var diaURL = 'https://www.googleapis.com/language/diacritize/v1?key=' + apiKey +
-              '&lang=ar&last_letter=false&prettyprint=false&message=' + trText + '&callback=diacritResponse';
+              '&lang=ar&last_letter=false&prettyprint=false&message=' + trText +
+              '&callback=callbackDiacrit';
 
           $.get(diaURL, function() {}, 'jsonp' );
       }
 
-
-      function startTranslating() {
-          var targetLang = 'fa';
-          var sourceText = escape($("#sourceText").val());
+      //translate src text from srcLang to dstLang, and then run the callback
+      function tr(src, callbackName) {
           var trURL = 'https://www.googleapis.com/language/translate/v2?key=' + apiKey +
-              '&source=en&target=' + targetLang + '&callback=translateText&q=' + sourceText;
-
+              '&source=' + srcLang + '&target=' + dstLang + '&callback=' + callbackName +
+              '&q=' + src;
           $.get(trURL, function() {}, 'jsonp'); 
       }
+
+      /////////////////////
+      //Functions for translating from Pinglish to English
+      ///////////////////////
+      
+      function translitPingToPr(str) {
+          //note that here the src language for transliteration is en, and dst is fa
+          //the 'opposite' of our larger goal of translating pinglish to english.
+          //so srcLang and dstLang are reversed
+          google.language.transliterate(
+              str.split('%20'), dstLang, srcLang,
+              function(result) {
+                  var textArray = result.transliterations.map(
+                      function(x) {
+                          return x.transliteratedWords[0];
+                      });
+                  var text = textArray.join(" ");
+                  $("#farsi").html(text);
+                  $("#diacritize").html("n/a");
+                  tr(text, 'callbackPrToEng');   
+                                        });
+      }
+
+      function callbackPrToEng(response) {
+          trText = response.data.translations[0].translatedText;
+          $("#goaltext").html(trText);
+      }
+
+      //////////////////////////////////
+      //the main function for translating the text
+      //////////////////////////////////
+      function startTranslating() {
+          var sourceText = escape($("#sourceText").val());
+
+          if (srcLang == 'en') {
+              tr(sourceText, 'callbackPrToPing');
+          } else {
+              translitPingToPr(sourceText);
+          }
+      }
+
       
     </script>
   </body>
